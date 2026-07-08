e937e7d57819924e2fb21304f4a747b2f2a98087
v-w04 <victor.walmart.04@gmail.com>
2026-07-08 11:01:34 -0600

    2

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@
 //   - Auto-update: cuando sube nueva versión, los dispositivos la reciben al reabrir
 // ============================================================================
 
-const CACHE_VERSION = 'em-checador-v574';
+const CACHE_VERSION = 'em-checador-v576';
 const CACHE_NAME    = CACHE_VERSION;
 
 const APP_SHELL = [
