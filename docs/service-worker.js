c3d2e89c8238cbee111667c439387ee2a4411432
v-w04 <victor.walmart.04@gmail.com>
2026-09-22 15:42:57 -0600

    Actualiza Checador

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -15,7 +15,7 @@
 //      y las manda solo, aunque la app esté cerrada.
 // ============================================================================
 
-var CACHE_NAME = 'em-checador-v718';
+var CACHE_NAME = 'em-checador-v720';
 
 var GAS_URL = 'https://script.google.com/macros/s/AKfycbxWu65gJ3jIbRp9WIbvNjia9IFsDJORUggDNyYUUQA_JxLYsbYjsawynN9hbV1kPqU5/exec';
 
